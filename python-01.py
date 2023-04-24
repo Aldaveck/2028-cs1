4cd427c93bfcf2caac623227c6730665b6829cb7
Aldaveck <asfajardo2028@clc.pshs.edu.ph>
2023-04-24 13:31:13 +0000

    my first python script for cs1

---
+++ python-01.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+## Aleck David Fajardo: Pisay CLC CS1 / 20230424
+
+num1 = float(input("Enter n1:"))
+num2 = float(input("Enter n2:"))
+num3 = float(input("Enter n3:"))
+num4 = float(input("Enter n4:"))
+num5 = float(input("Enter n5:"))
+num6 = float(input("Enter n6:"))
+num7 = float(input("Enter n7:"))
+num8 = float(input("Enter n8:"))
+num9 = float(input("Enter n9:"))
+num10 = float(input("Enter n10:"))
+
+
+print(" ")
+
+total = num1 + num2 + num3+ num4 +num5 +num6 +num7 +num8 +num9 +num10
+
+average = total / 10
+
+
+
+print(f"Total:{total}")
+
+print(f"Average:{average}")
