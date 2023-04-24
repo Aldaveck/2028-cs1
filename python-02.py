6090a682466955df1742ddffa75a4d7abda24004
Aldaveck <asfajardo2028@clc.pshs.edu.ph>
2023-04-24 14:01:15 +0000

    my second python script for cs1

--- python-02.py
+++ python-02.py
@@ -5,27 +5,15 @@
 # Aleck David Fajardo  Q4      04/24/2023
 # 7 - Sapphire        FA1      Ms. Jona Tibay
 
+total = 0
+count = 1
 
-num1 = float(input("Enter n1:"))
-num2 = float(input("Enter n2:"))
-num3 = float(input("Enter n3:"))
-num4 = float(input("Enter n4:"))
-num5 = float(input("Enter n5:"))
-num6 = float(input("Enter n6:"))
-num7 = float(input("Enter n7:"))
-num8 = float(input("Enter n8:"))
-num9 = float(input("Enter n9:"))
-num10 = float(input("Enter n10:"))
-
-
-print(" ")
-
-total = num1 + num2 + num3+ num4 +num5 +num6 +num7 +num8 +num9 +num10
+while count <= 10:
+    num = float(input(f"Enter n{count}: "))
+    total += num
+    count += 1
 
 average = total / 10
 
-
-
-print(f"Total:{total}")
-
-print(f"Average:{average}")
+print(f"\nTotal: {total}")
+print(f"Average: {average}")
